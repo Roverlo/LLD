1981ae1644436b31043969e92ce988fc12296281
Roverlo <lfw2400@gmail.com>
2025-08-27 17:39:23 +0800

    chore: 移除BKUP2025备份目录（不再需要）

--- BKUP2025/src/main/worker.js
+++
@@ -1,9 +0,0 @@
-const { workerData, parentPort } = require('worker_threads');
-const { generatePlan } = require('./generator.js');
-
-try {
-    const plan = generatePlan(workerData);
-    parentPort.postMessage({ success: true, plan });
-} catch (error) {
-    parentPort.postMessage({ success: false, error: error.message });
-}
